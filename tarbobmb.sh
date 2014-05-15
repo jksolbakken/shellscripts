6232aac9f02c6b34874191128c0a5bede5fe3078
J-K. Solbakken <jksolbakken@gmail.com>
2014-05-15 18:04:58 +0200

    typo

--- tarbobmb.sh
+++ tarbobmb.sh
@@ -1,4 +1,3 @@
 function tarbomb(){ 
-   [[ $( tar tf "$1" |sed 's,^\./,,' |awk -F/ '{print $1}' |sort |uniq |wc -l ) -eq 1 ]] 
-     && echo "OK" || echo 'Tarbomb!'; 
+   [[ $( tar tf "$1" |sed 's,^\./,,' |awk -F/ '{print $1}' |sort |uniq |wc -l ) -eq 1 ]]  && echo "OK" || echo 'Tarbomb!'; 
 }
