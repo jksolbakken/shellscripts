173317bcd1fdaba6074d6ed1f35280cb87e48dbf
J-K. Solbakken <j.k.solbakken@fsat.no>
2017-02-15 11:28:38 +0100

    brew upgrade

--- aliases.sh
+++ aliases.sh
@@ -3,3 +3,5 @@ alias tot='du -cksh'
 alias rmdocker='docker rm $(docker ps -a -q) && docker rmi $(docker images -q)'
 
 alias weather='curl wttr.in/Oslo'
+
+alias brup='brew update && brew upgrade && brew cleanup'
