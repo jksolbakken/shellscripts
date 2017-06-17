b3a135b6c3bf233d33536e4f4b99e36873280bd3
J-K. Solbakken <jksolbakken@gmail.com>
2017-06-17 13:46:55 +0200

    alias gw for ./gradlew

--- aliases.sh
+++ aliases.sh
@@ -5,3 +5,5 @@ alias rmdocker='docker rm $(docker ps -a -q) && docker rmi $(docker images -q)'
 alias weather='curl wttr.in/Oslo'
 
 alias brup='brew update && brew upgrade && brew cleanup'
+
+alias gw='./gradlew'
