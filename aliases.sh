fc36579cd9026d6234ed2edc58e4e3f0efaece94
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2019-05-21 15:53:33 +0200

    alias for socks-proxed curl

--- aliases.sh
+++ aliases.sh
@@ -21,3 +21,5 @@ alias myip='ifconfig | grep "inet " | grep -Fv 127.0.0.1'
 alias restartaudio='sudo kill `ps -ax | grep coreaudiod | grep sbin | awk "{print $1}"`'
 
 alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
+
+alias proxcurl='HTTPS_PROXY="socks5://localhost:14122" && curl
