1cb571ac9c5f08e8041ef8a0317cd0b196c015d4
J-K. Solbakken <jk@Jan-Kares-MBP.lan>
2020-07-10 21:12:21 +0200

    resolve name conflict

--- aliases.sh
+++ aliases.sh
@@ -16,11 +16,11 @@ alias k="kubectl"
 
 alias sniff='sudo lsof -iTCP -sTCP:LISTEN -n -P'
 
-alias myip='ifconfig | grep "inet " | grep -Fv 127.0.0.1'
+alias localip='ifconfig | grep "inet " | grep -Fv 127.0.0.1'
 
 alias restartaudio='sudo kill `ps -ax | grep coreaudiod | grep sbin | awk "{print $1}"`'
 
-alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias externalip="dig +short myip.opendns.com @resolver1.opendns.com"
 
 alias proxcurl='HTTPS_PROXY="socks5://localhost:14122" curl'
 
