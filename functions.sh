26166b4a41440cdb321d83c4e932b1af4ef6bdd2
J-K. Solbakken <jksolbakken@gmail.com>
2021-02-01 12:53:05 +0100

    sha-verify convenience functions

--- functions.sh
+++ functions.sh
@@ -1,3 +1,15 @@
 klogf () {
 	kubectl logs -f deployment/$1 --all-containers
 }
+
+sha256verify() {
+    echo "$1 $2" | sha256sum --check
+}
+
+sha384verify() {
+    echo "$1 $2" | sha384sum --check
+}
+
+sha256verify() {
+    echo "$1 $2" | sha384sum --check
+}
